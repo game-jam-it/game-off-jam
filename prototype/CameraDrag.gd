4e4034bc8a5f5cf579f6eb5df169af18093b21ee
Koen Van Rulo <koen.vr@vecno.io>
2022-11-16 11:24:47 +0100

    cleanup camera zoom behaviour

--- prototype/CameraDrag.gd
+++ prototype/CameraDrag.gd
@@ -6,6 +6,12 @@ var zoom_in = Vector2(1.0, 1.0)
 var focus_on = Vector2(7.5, 7.5)
 var zoom_out = Vector2(15.0, 15.0)
 
+onready var viewport_offset = get_viewport().size * 0.5
+onready var viewport_transform = get_viewport_transform()
+
+func _ready():
+	zoom_reset()
+	
 func _input(event):
 	if event.is_action_pressed("mouse_pan"):
 		panning = true
@@ -15,17 +21,17 @@ func _input(event):
 		offset -= event.relative * zoom
 
 func zoom_reset():
-	offset = get_viewport().size * 0.5
 	zoom = self.zoom_out
+	offset = get_viewport().size * 0.5
+	viewport_offset = get_viewport().size * 0.5
+	viewport_transform = get_viewport_transform()
 
 func zoom_to(location:Vector2):
-	var target = get_viewport_transform() * location
-	offset = get_viewport().size * 0.5
-	offset -= (offset - target) * 15.0 * 2.0
 	zoom = self.zoom_in
+	var target = viewport_transform * location
+	offset = viewport_offset - (viewport_offset - target) * 15
 
 func focus_on(location:Vector2):
-	var target = get_viewport_transform() * location
-	offset = get_viewport().size * 0.5
-	offset -= (offset - target) * 7.5 * 1.5
 	zoom = self.focus_on 
+	var target = viewport_transform * location
+	offset = viewport_offset - (viewport_offset - target) * 15
