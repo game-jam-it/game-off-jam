49a2ecb97d1b43e45152ca0b86ab248561bf67eb
Koen Van Rulo <koen.vr@vecno.io>
2022-11-20 22:41:31 +0100

    swap popup for control
    Implement manual behaviour to avoid UI input locks.

--- UserInterface/TownHud/prefabs/ExpeditionDialog.gd
+++ UserInterface/TownHud/prefabs/ExpeditionDialog.gd
@@ -1,4 +1,4 @@
-extends Popup
+extends Control
 
 var coords = null
 
@@ -12,10 +12,6 @@ onready var start_btn = get_node('%ExploreButton')
 onready var cancel_btn = get_node('%LeaveButton')
 
 func _ready():
-	self.set_exclusive(true)
-	connect("popup_hide", self, "on_hide_popup")
-	connect("about_to_show", self, "on_about_to_show")
-
 	start_btn.connect("pressed", self, "on_start_pressed")
 	cancel_btn.connect("pressed", self, "on_cancel_pressed")
 
@@ -24,6 +20,7 @@ func _input(input):
 		return
 	if input.is_action_pressed("ui_cancel"):
 		self.visible = false
+		emit_signal("cancel_expedition")
 
 func set_info(title, descr):
 	title_txt.text = title
@@ -34,10 +31,9 @@ func on_start_pressed():
 
 func on_cancel_pressed():
 	self.visible = false
-
-func on_hide_popup():
 	emit_signal("cancel_expedition")
 
-func on_about_to_show():
+func popup():
+	self.visible = true
 	yield(get_tree(), "idle_frame")
 	start_btn.grab_focus()
