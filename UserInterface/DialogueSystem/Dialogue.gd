a7b7cabb01e2bec2729d6f086e56d6c8218617e7
marcbosmancode <61054760+marcbosmancode@users.noreply.github.com>
2022-11-23 22:50:41 +0100

    Adding dialogue system singleton
    Being able to create new dialogue boxes from anywhere. They will be added as a child of Overlay

--- UserInterface/DialogueSystem/Dialogue.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-class_name Dialogue
-
-const scripts = {
-	"intro": {
-		1: ["Main character", "Hello this is a dialogue"],
-		2: ["Someone else", "Can change the person speaking!"]
-	}
-}
