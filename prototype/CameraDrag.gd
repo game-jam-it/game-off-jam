61c0d8a04b044b5974e531c3ba9c1f7deac84f26
Koen Van Rulo <koen.vr@vecno.io>
2022-11-24 02:28:07 +0100

    fix camera zoom

--- prototype/CameraDrag.gd
+++ prototype/CameraDrag.gd
@@ -9,6 +9,8 @@ var zoom_in = Vector2(0.75, 0.75)
 var focus_on = Vector2(7.5, 7.5)
 var zoom_out = Vector2(15.0, 15.0)
 
+var zoom_last =Vector2(15.0, 15.0)
+
 export var init_zoom = Vector2(20.0, 20.0)
 export var init_offset = Vector2(0.0, 256.0)
 
@@ -18,6 +20,7 @@ onready var viewport_transform = get_viewport_transform()
 
 func _ready():
 	get_viewport().connect("size_changed", self, "_on_size_changed")
+	viewport_ratio = Vector2(BASE_X, BASE_Y) / get_viewport().size
 	zoom_init()
 	
 func _input(event):
@@ -30,26 +33,33 @@ func _input(event):
 
 func zoom_init():
 	zoom = self.init_zoom
+	zoom_last = self.init_zoom
 	offset = self.init_offset + (get_viewport().size * 0.5)
 	viewport_offset = get_viewport().size * 0.5
 	viewport_transform = get_viewport_transform()
 
 func zoom_reset():
 	zoom = self.zoom_out
+	zoom_last = self.zoom_out
 	offset = get_viewport().size * 0.5
 	viewport_offset = get_viewport().size * 0.5
 	viewport_transform = get_viewport_transform()
 
 func _on_size_changed():
+	zoom = zoom_last
+	offset = get_viewport().size * 0.5
+	viewport_ratio = Vector2(BASE_X, BASE_Y) / get_viewport().size
 	viewport_offset = get_viewport().size * 0.5
 	viewport_transform = get_viewport_transform()
 
 func set_zoom_to(location:Vector2):
 	zoom = self.zoom_in
+	zoom_last = self.zoom_in
 	var target = viewport_transform * location
-	offset = viewport_offset - (viewport_offset - target) * 15
+	offset = viewport_offset - (viewport_offset - target) * (15 * viewport_ratio)
 
 func set_focus_to(location:Vector2):
-	zoom = self.focus_on 
+	zoom = self.focus_on
+	zoom_last = self.focus_on
 	var target = viewport_transform * location
-	offset = viewport_offset - (viewport_offset - target) * 15
+	offset = viewport_offset - (viewport_offset - target) * (15 * viewport_ratio)
