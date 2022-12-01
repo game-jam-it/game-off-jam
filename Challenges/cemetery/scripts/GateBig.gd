5c9280d4a043adc5ea6aeb9dd860919614da6e8d
Koen Van Rulo <koen.vr@vecno.io>
2022-12-01 21:39:51 +0100

    temp gate fix

--- Challenges/cemetery/scripts/GateBig.gd
+++ Challenges/cemetery/scripts/GateBig.gd
@@ -1,9 +1,14 @@
 extends Challenge
 
 func reward(player: PlayerActor) -> EntityAction:
-	print("%s missing overwrite of the challenge.reward() method" % name)
-	return null
+	if player == null: 
+		return null
+	var hex = self.get_grid_cell()
+	return player.get_move_to(hex)
 
 func penalty(player: PlayerActor) -> EntityAction:
-	print("%s missing overwrite of the challenge.penalty() method" % name)
-	return null
+	if player == null: 
+		return null
+	player.take_damage(1)
+	var hex = self.get_grid_cell()
+	return player.get_move_to(hex)
