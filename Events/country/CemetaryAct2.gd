f998dd663a6fbfa699943f5eca38f92fd7d52cc2
Koen Van Rulo <koen.vr@vecno.io>
2022-12-01 21:28:42 +0100

    add new boss Bones

--- Events/country/CemetaryAct2.gd
+++ Events/country/CemetaryAct2.gd
@@ -1,17 +1,17 @@
 extends ExpeditionEvent
 
-onready var mallus = get_node("%Mallus")
+onready var bones = get_node("%Bones")
 onready var player = get_node("%PlayerActor")
 
 func _ready():
-	mallus.connect("enemy_died", self, "_on_mallus_died")
+	bones.connect("enemy_died", self, "_on_bones_died")
 
 func get_cam_target():
 	return player.global_position
 
-func _on_mallus_died(entity):
-	mallus.disconnect("enemy_died", self, "_on_mallus_died")
-	print("[%s] TODO _on_mallus_died events" % [name])
+func _on_bones_died(entity):
+	bones.disconnect("enemy_died", self, "_on_bones_died")
+	print("[%s] TODO _on_bones_died events" % [name])
 	# TODO: Dimm the glow on the fontain
 	# TODO: Mark Points of Intrest
 	# TODO: Kill all others
